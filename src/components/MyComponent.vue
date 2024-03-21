18f3e9660f245424dd9e4d57289d57bc9e2fb0fd
LynchQGit <hlq_git@163.com>
2024-03-21 15:57:25 +0800

    组件添加交互性

--- src/components/MyComponent.vue
+++ src/components/MyComponent.vue
@@ -1,12 +1,25 @@
 <template>
     <div>
         <p>This is a component</p>
+        <!-- 添加一个按钮：反转消息 -->
+        <p>{{ message }}</p>
+        <button @click="reverseMessage">点我反转消息</button>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'MyComponent'
+    name: 'MyComponent',
+    data() {
+        return {
+            message: 'Hello Vue!'
+        }
+    },
+    methods: {
+        reverseMessage() {
+            this.message = this.message.split('').reverse().join('')
+        }
+    },
 }
 </script>
 
